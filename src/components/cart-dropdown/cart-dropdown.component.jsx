305f705e24131349ab1f20e96e7f9fb45ab5ea67
Dave Lin <dave.lin21@gmail.com>
2022-05-22 18:46:38 -0400

    checkout page and functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -10,13 +11,18 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
   return (
     <div className='cart-dropdown-container'>
       {cartItems.map((item) => {
         return <CartItem key={item.id} cartItem={item} />;
       })}
-      <Button>Checkout</Button>
+      <Button onClick={goToCheckoutHandler}>Checkout</Button>
     </div>
   );
 };
